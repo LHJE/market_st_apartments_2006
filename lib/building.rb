5c239dc66c8ef4007508a9006b633767e0372dfb
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-14 10:58:47 -0600

    Didn't get the .renters to work.

--- lib/building.rb
+++ lib/building.rb
@@ -25,10 +25,17 @@ class Building
 # I get that this this is somehow supposed to access the first object in the units array, but I just can't figure out how.     @building.units.????.renter?
 # It's either that or something like what I've got below, but it keeps saying it doesn't recognize 'units' or 'name'
 
+  # def renters
+  #   units.each do |tenet|
+  #     tenet.renter[0]
+  #   end
+  #   #gah.  Why is this just giving me exactly what .units is giving me?
+  # end
+
+
   def renters
-    units.each do |tenet|
-      tenet.renter[0]
+    units.map do |tenet|
+      tenet.shift  #it's telling me `.shift` and `.first` aren't methods and that has me more baffled than ever before.
     end
-    #gah.  Why is this just giving me exactly what .units is giving me?
   end
 end
